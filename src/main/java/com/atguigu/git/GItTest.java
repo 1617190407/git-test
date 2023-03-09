1b03e57d2927f6266f7640e83e77295919751e89
Layne <Layne@atguigu.com>
2023-03-09 16:07:39 +0800

    push master

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -3,7 +3,6 @@ package com.atguigu.git;
 public class GItTest {
     public static void main( String[] args ) {
         System.out.println("hell git!!!!");
-        System.out.println("hell git!!!!");
         System.out.println("hell git6666");
         System.out.println("hell?");
         System.out.println("hell git2423424");
