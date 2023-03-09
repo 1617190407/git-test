72cd716859348418a57d9f446ddf0f6cb5aae784
Layne <Layne@atguigu.com>
2023-03-09 10:14:41 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -4,5 +4,6 @@ public class GItTest {
     public static void main( String[] args ) {
         System.out.println("hell git!!!!");
         System.out.println("hell git!!!!");
+        System.out.println("hell git6666");
     }
 }
